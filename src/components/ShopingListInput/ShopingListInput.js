303749652b031a6eb16a5db817c5debfb83acd44
Abidur-Rahman-Chowdhury <abid43iiuc@gmail.com>
2022-11-10 00:44:28 +0600

    done adding shoppingList functionality

--- src/components/ShopingListInput/ShopingListInput.js
+++ src/components/ShopingListInput/ShopingListInput.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 const ShopingListInput = () => {
+    const dispatch = useDispatch();
+    const [shoppingList, setShoppingList] = useState();
+    
+    const handleChange = (event) => setShoppingList(event.target.value);
+    const handleClick = () => dispatch({
+        type: 'ADD',
+        payload:shoppingList,
+    })
     return (
         <>
-            <input type="text" /> 
-            <input type="button" value="Add" />
+            <input onChange={handleChange} type="text" /> 
+            <input onClick={handleClick} type="button" value="Add" />
         </>
     );
 };
