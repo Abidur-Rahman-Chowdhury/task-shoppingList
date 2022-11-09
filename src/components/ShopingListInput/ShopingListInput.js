cfa2c6c864c5daa9debddd33d4791a2c4c9235c7
Abidur-Rahman-Chowdhury <abid43iiuc@gmail.com>
2022-11-10 00:28:18 +0600

    done adding component in app.js file

---
+++ src/components/ShopingListInput/ShopingListInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ShopingListInput = () => {
+    return (
+        <>
+            <input type="text" /> 
+            <input type="button" value="Add" />
+        </>
+    );
+};
+
+export default ShopingListInput;
---
+++ src/components/ShowList/ShowShoppingList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShowShoppingList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ShowShoppingList;
